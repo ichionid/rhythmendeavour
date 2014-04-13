ed2ed48edb241d6060c5a61b878abcad3e0275bb
Giannis Chionidis <gc@kirkeweb.dk>
2014-04-13 12:32:21 +0200

    Initial commit. Adding instruments. jMusic.jar added externally.

---
+++ rhythmgeneration/generators/RhythmAutomataGenerator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rhythmgeneration.generators;
+
+/**
+ *
+ * @author lebowski
+ */
+public class RhythmAutomataGenerator {
+    
+}
