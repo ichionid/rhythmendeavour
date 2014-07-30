9b1c10fceaa1d2539829c112496c70ef4ec9acfe
Giannis Chionidis <gc@kirkeweb.dk>
2014-07-30 18:24:04 +0300

    Adding gitignore and finishing up cellularAutomata generator

--- src/rhythmgeneration/generators/RhythmAutomataGenerator.java
+++ src/rhythmgeneration/generators/RhythmAutomataGenerator.java
@@ -17,7 +17,7 @@ public class RhythmAutomataGenerator {
      * The function returns a pattern when either maxIterations or maxDistance
      * is reached.
      * 
-     * @param input an ArrayList of <code>Booleans</code> where TRUE stands for
+     * @param inputPattern an ArrayList of <code>Booleans</code> where TRUE stands for
      * onset and FALSE for offset.
      * @param rules,  an ArrayList of <code>Strings</code> where every string 
      * contains only a single character.
@@ -28,9 +28,90 @@ public class RhythmAutomataGenerator {
      * @return an ArrayList of <code>Booleans</code> where TRUE stands for
      * onset and FALSE for offset
      */
-    public ArrayList<Boolean> voiceGenerator(ArrayList<Boolean> input, 
+    public ArrayList<Boolean> voiceGenerator(ArrayList<Boolean> inputPattern, 
             ArrayList<String> rules, int maxIterations, int maxDistance ) {
-        return input;
+        ArrayList<Boolean> outputRhythm = new ArrayList();
+        outputRhythm = inputPattern;
+        for (int i = 0; i < maxIterations; i++) {
+            outputRhythm = generatePattern(outputRhythm, rules);
+        }
+        return outputRhythm;
     }
-        
+    
+    /**
+     * 
+     * @param inputPattern an ArrayList of <code>Booleans</code> where TRUE stands for
+     * onset and FALSE for offset.
+     * @param rules,  an ArrayList of <code>Strings</code> where every string 
+     * contains only a single character.
+     * @return an ArrayList of <code>Booleans</code> where TRUE stands for
+     * onset and FALSE for offset
+     */    
+    public ArrayList<Boolean> generatePattern(ArrayList<Boolean> inputPattern, 
+            ArrayList<String> rules) {
+        ArrayList<Boolean> outputRhythm = new ArrayList();
+        int neighborsSum;
+        for (int i = 0; i < inputPattern.size(); i++) {
+            neighborsSum = 0;
+            if (i==0) {
+                // First cell
+                if(inputPattern.get(0)) {
+                    neighborsSum++;
+                }
+                try {
+                  if(inputPattern.get(1)) {
+                        neighborsSum++;
+                  }
+                } catch (IndexOutOfBoundsException e) {
+                    //Suppress an out of bounds exceptiond
+                }
+            } else if(i == (inputPattern.size()-1)) {
+                // Last cell
+                if(inputPattern.get(i)) {
+                    neighborsSum++;
+                }
+                if(inputPattern.get(i-1)) {
+                    neighborsSum++;
+                }
+            }
+            else {
+                if(inputPattern.get(i)) {
+                    neighborsSum++;
+                }
+                if(inputPattern.get(i-1)) {
+                    neighborsSum++;
+                }
+                if(inputPattern.get(i+1)) {
+                    neighborsSum++;
+                }
+            }
+            outputRhythm.set(i, calculateCellValue(inputPattern.get(i), neighborsSum, rules));
+        }
+        return outputRhythm;
+    }
+   /**
+     * 
+     * @param initialValue a <code>Boolean</code> representing the initial 
+     * value of the note. 
+     * @param neighborsSum an <code>Int</code> representing the sum of all 
+     * neighboring onsets. 
+     * @param rules, an ArrayList of <code>Strings</code> where every string 
+     * contains only a single character.
+     * @return an ArrayList of <code>Booleans</code> where TRUE stands for
+     * onset and FALSE for offset
+     */
+    public Boolean calculateCellValue(Boolean initialValue, int neighborsSum, ArrayList<String> rules) {
+        switch (rules.get(neighborsSum)) {
+           case "N":
+                 return true;
+           case "R": 
+                 return false;
+           case "F":
+                 return !initialValue;
+           case "U":
+                 return initialValue;
+           default:
+               return initialValue;
+       }
+    }
 }
